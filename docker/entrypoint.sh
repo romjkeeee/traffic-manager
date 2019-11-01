9cac8e5a9c4389333e2cb40d683da41a2472dd88
Alex Cherkes <cherkes.k3@gmail.com>
2019-11-01 13:12:30 +0200

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,15 +6,15 @@ cd /var/www/
 composer install
 npm install
 
+
 php artisan migrate
 php view:clear
 php route:clear
 php config:clear
 php cache:clear
+php artisan adminlte:install
 
 npm run dev
 
 chmod -R 777 /var/www/*
 chown -R www-data:www-data /var/www*
-
-
