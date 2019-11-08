7d7d92f8003f477e5166bda94f28eef2852680aa
Roman.s <romjkeeeen@gmail.com>
2019-11-08 13:26:40 +0200

    admin

--- database/migrations/2019_11_06_105617_add_relationships_to_users_table.php
+++ database/migrations/2019_11_06_105617_add_relationships_to_users_table.php
@@ -14,7 +14,7 @@ class AddRelationshipsToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedInteger('role_id')->index();
+            $table->unsignedInteger('role_id')->index()->default('1')->change();
             $table->foreign('role_id')->references('id')->on('roles');
         });
     }
