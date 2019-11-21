315864e6955e72c9794c431e6d8264fd575e5c87
Roman.s <romjkeeeen@gmail.com>
2019-11-21 16:12:49 +0200

    REF add param

--- resources/views/pages/offers/edit.blade.php
+++ resources/views/pages/offers/edit.blade.php
@@ -71,7 +71,7 @@
 
                     <div class="form-group">
                         <label for="inputForadd_param">@lang('adminlte.add_param')</label>
-                        <input class="form-control" value="{{ $data->add_param }}" type="number" step="1" min="0" style="" name="add_param" id="add_param" >
+                        <input class="form-control" value="{{ $data->add_param }}" name="add_param" id="add_param" >
                     </div>
 
                     <div class="form-group">
